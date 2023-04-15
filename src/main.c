b820112526020133f21e5d39c1b51c98a79bd09c
Andrew Carey <andrewcarey@usf.edu>
2023-04-15 18:37:59 -0400

    updated with non working aes

--- src/main.c
+++ src/main.c
@@ -45,6 +45,15 @@
 #include "retained.h"
 #include <hal/nrf_gpio.h>
 
+//crypto
+#include <stdlib.h>
+#include <psa/crypto.h>
+#include <psa/crypto_extra.h>
+
+#ifdef CONFIG_BUILD_WITH_TFM
+#include <tfm_ns_interface.h>
+#endif
+
 #define BUSY_WAIT_S 2U
 #define SLEEP_S 2U
 
@@ -118,7 +127,7 @@ static const struct gpio_dt_spec Output_Other = GPIO_DT_SPEC_GET(Output_Other_Pi
 #define towerSizeTAC 12
 #define towerSizeTA 12
 #define towerSizeCOPS 25
-#define towerSizeSNR 300
+#define towerSizeSNR 400
 
 //towers
 
@@ -754,6 +763,356 @@ void empty(void)
 
 }
 
+/******
+ * AES-GCM
+ * 
+ * CRYPTO!!!!!!
+*/
+
+//get some definitions out of the way
+#define APP_SUCCESS             (0)
+#define APP_ERROR               (-1)
+#define APP_SUCCESS_MESSAGE "Example finished successfully!"
+#define APP_ERROR_MESSAGE "Example exited with error!"
+
+#define NRF_CRYPTO_EXAMPLE_AES_MAX_TEXT_SIZE (2000)
+#define NRF_CRYPTO_EXAMPLE_AES_BLOCK_SIZE (16)
+#define NRF_CRYPTO_EXAMPLE_AES_IV_SIZE (12)
+#define NRF_CRYPTO_EXAMPLE_AES_ADDITIONAL_SIZE (35)
+#define NRF_CRYPTO_EXAMPLE_AES_GCM_TAG_LENGTH (16)
+
+#define PRINT_HEX(p_label, p_text, len)				  \
+	({							  \
+		LOG_INF("---- %s (len: %u): ----", p_label, len); \
+		LOG_HEXDUMP_INF(p_text, len, "Content:");	  \
+		LOG_INF("---- %s end  ----", p_label);		  \
+	})
+
+//I am not using this in production, just a temporary fix :p
+/* AES sample IV, DO NOT USE IN PRODUCTION */
+static uint8_t m_iv[NRF_CRYPTO_EXAMPLE_AES_IV_SIZE];
+
+uint8_t m_plain_text[NRF_CRYPTO_EXAMPLE_AES_MAX_TEXT_SIZE];
+
+static uint8_t m_additional_data[NRF_CRYPTO_EXAMPLE_AES_ADDITIONAL_SIZE] = {
+	"Example string of additional data"
+};
+
+static uint8_t m_encrypted_text[NRF_CRYPTO_EXAMPLE_AES_MAX_TEXT_SIZE +
+				NRF_CRYPTO_EXAMPLE_AES_GCM_TAG_LENGTH];
+
+static uint8_t m_decrypted_text[NRF_CRYPTO_EXAMPLE_AES_MAX_TEXT_SIZE];
+
+static psa_key_handle_t key_handle;
+
+int crypto_init(void)
+{
+	psa_status_t status;
+
+	/* Initialize PSA Crypto */
+	status = psa_crypto_init();
+	if (status != PSA_SUCCESS) {
+		return APP_ERROR;
+	}
+
+	return APP_SUCCESS;
+}
+
+int crypto_finish(void)
+{
+	psa_status_t status;
+
+	/* Destroy the key handle */
+	status = psa_destroy_key(key_handle);
+	if (status != PSA_SUCCESS) {
+		//LOG_INF("psa_destroy_key failed! (Error: %d)", status);
+		return APP_ERROR;
+	}
+
+	return APP_SUCCESS;
+}
+
+int generate_key(void)
+{
+	psa_status_t status;
+
+	//LOG_INF("Generating random AES key...");
+
+	/* Configure the key attributes */
+	psa_key_attributes_t key_attributes = PSA_KEY_ATTRIBUTES_INIT;
+
+	psa_set_key_usage_flags(&key_attributes, PSA_KEY_USAGE_ENCRYPT | PSA_KEY_USAGE_DECRYPT);
+	psa_set_key_lifetime(&key_attributes, PSA_KEY_LIFETIME_VOLATILE);
+	psa_set_key_algorithm(&key_attributes, PSA_ALG_GCM);
+	psa_set_key_type(&key_attributes, PSA_KEY_TYPE_AES);
+	psa_set_key_bits(&key_attributes, 128);
+
+	/* Generate a random key. The key is not exposed to the application,
+	 * we can use it to encrypt/decrypt using the key handle
+	 */
+	status = psa_generate_key(&key_attributes, &key_handle);
+	if (status != PSA_SUCCESS) {
+		//LOG_INF("psa_generate_key failed! (Error: %d)", status);
+		return APP_ERROR;
+	}
+
+	/* After the key handle is acquired the attributes are not needed */
+	psa_reset_key_attributes(&key_attributes);
+
+	//LOG_INF("AES key generated successfully!");
+
+	return 0;
+}
+
+int encrypt_aes_gcm(void)
+{
+	uint32_t output_len;
+	psa_status_t status;
+
+	//LOG_INF("Encrypting using AES GCM MODE...");
+
+	/* Generate a random IV */
+	status = psa_generate_random(m_iv, NRF_CRYPTO_EXAMPLE_AES_IV_SIZE);
+	if (status != PSA_SUCCESS) {
+		//LOG_INF("psa_generate_random failed! (Error: %d)", status);
+		return APP_ERROR;
+	}
+
+	/* Encrypt the plaintext and create the authentication tag */
+	status = psa_aead_encrypt(key_handle,
+				  PSA_ALG_GCM,
+				  m_iv,
+				  sizeof(m_iv),
+				  m_additional_data,
+				  sizeof(m_additional_data),
+				  m_plain_text,
+				  sizeof(m_plain_text),
+				  m_encrypted_text,
+				  sizeof(m_encrypted_text),
+				  &output_len);
+	if (status != PSA_SUCCESS) {
+		//LOG_INF("psa_aead_encrypt failed! (Error: %d)", status);
+		return APP_ERROR;
+	}
+
+	//LOG_INF("Encryption successful!");
+	PRINT_HEX("IV", m_iv, sizeof(m_iv));
+	PRINT_HEX("Additional data", m_additional_data, sizeof(m_additional_data));
+	PRINT_HEX("Plaintext", m_plain_text, sizeof(m_plain_text));
+	PRINT_HEX("Encrypted text", m_encrypted_text, sizeof(m_encrypted_text));
+
+	return APP_SUCCESS;
+}
+
+int decrypt_aes_gcm(void)
+{
+	uint32_t output_len;
+	psa_status_t status;
+
+	//LOG_INF("Decrypting using AES GCM MODE...");
+
+	/* Decrypt and authenticate the encrypted data */
+	status = psa_aead_decrypt(key_handle,
+				  PSA_ALG_GCM,
+				  m_iv,
+				  sizeof(m_iv),
+				  m_additional_data,
+				  sizeof(m_additional_data),
+				  m_encrypted_text,
+				  sizeof(m_encrypted_text),
+				  m_decrypted_text,
+				  sizeof(m_decrypted_text),
+				  &output_len);
+	if (status != PSA_SUCCESS) {
+		//LOG_INF("psa_aead_decrypt failed! (Error: %d)", status);
+		return APP_ERROR;
+	}
+
+	PRINT_HEX("Decrypted text", m_decrypted_text, sizeof(m_decrypted_text));
+
+	/* Check the validity of the decryption */
+	if (memcmp(m_decrypted_text, m_plain_text, NRF_CRYPTO_EXAMPLE_AES_MAX_TEXT_SIZE) != 0) {
+		//LOG_INF("Error: Decrypted text doesn't match the plaintext");
+		return APP_ERROR;
+	}
+
+	//LOG_INF("Decryption and authentication successful!");
+
+	return APP_SUCCESS;
+}
+
+//custom crypto
+
+//start crypto
+int startCrypto(void)
+{
+	int status;
+
+	status = crypto_init();
+	if (status != 0) {
+		//LOG_INF(APP_ERROR_MESSAGE);
+		//return -1;
+	}
+
+	status = generate_key();
+	if (status != 0) {
+		//LOG_INF(APP_ERROR_MESSAGE);
+		//return -1;
+	}
+
+
+	return 0;
+}
+
+int encryptData(char *str, int strLength)
+{
+	uint8_t * u = (uint8_t *)(str);
+	int loop = 0;
+	for(loop = 0; loop < strLength; loop++)
+	{
+		m_plain_text[loop] = u[loop];
+	}
+	
+
+	int status;
+	
+	status = encrypt_aes_gcm();
+	if (status != 0) {
+		//LOG_INF(APP_ERROR_MESSAGE);
+		return -1;
+	}
+
+	printk("%d",m_plain_text);
+	printk("\n");
+	printk("%d",m_encrypted_text);
+	printk("\n");
+
+	return status;
+}
+
+int decryptData()
+{
+	int status;
+
+	status = decrypt_aes_gcm();
+	if (status != 0) {
+		//LOG_INF(APP_ERROR_MESSAGE);
+		return -1;
+	}
+
+	printk("%d", m_plain_text);
+	printk("\n");
+	printk("%d", m_decrypted_text);
+	printk("\n");
+
+	return 0;
+}
+
+int cryptoEnd()
+{
+	int status;
+	status = crypto_finish();
+	if (status != 0) {
+		//LOG_INF(APP_ERROR_MESSAGE);
+		return -1;
+	}
+
+	return 0;
+}
+
+/**
+ * print all the tower data to a giant AES string
+ * 
+ */
+
+int secure()
+{
+	char all[500];
+
+	int loop;
+	strcpy(all, "\n");
+	strcat(all, separator);
+	strcat(all, "\n");
+
+	strcat(all, t1ID);
+	strcat(all, "\n");
+	strcat(all, t1MCC);
+	strcat(all, "\n");
+	strcat(all, t1MNC);
+	strcat(all, "\n");
+	strcat(all, t1TAC);
+	strcat(all, "\n");
+	strcat(all, t1TA);
+	strcat(all, "\n");
+	strcat(all, t1COPS);
+	strcat(all, "\n");
+	strcat(all, t1RSRP);
+	strcat(all, "\n");
+	strcat(all, t1RSRQ);
+	strcat(all, "\n");
+	strcat(all, t1SNR);
+	strcat(all, "\n");
+
+	strcat(all, t2ID);
+	strcat(all, "\n");
+	strcat(all, t2MCC);
+	strcat(all, "\n");
+	strcat(all, t2MNC);
+	strcat(all, "\n");
+	strcat(all, t2TAC);
+	strcat(all, "\n");
+	strcat(all, t2TA);
+	strcat(all, "\n");
+	strcat(all, t2COPS);
+	strcat(all, "\n");
+	strcat(all, t2RSRP);
+	strcat(all, "\n");
+	strcat(all, t2RSRQ);
+	strcat(all, "\n");
+	strcat(all, t2SNR);
+	strcat(all, "\n");
+
+	strcat(all, t3ID);
+	strcat(all, "\n");
+	strcat(all, t3MCC);
+	strcat(all, "\n");
+	strcat(all, t3MNC);
+	strcat(all, "\n");
+	strcat(all, t3TAC);
+	strcat(all, "\n");
+	strcat(all, t3TA);
+	strcat(all, "\n");
+	strcat(all, t3COPS);
+	strcat(all, "\n");
+	strcat(all, t3RSRP);
+	strcat(all, "\n");
+	strcat(all, t3RSRQ);
+	strcat(all, "\n");
+	strcat(all, t3SNR);
+	strcat(all, "\n");
+
+	strcat(all, time);
+	strcat(all, "\n");
+
+	strcat(all, separator);
+	strcat(all, "\n");
+
+	for(loop = 0; loop < strlen(all); loop++)
+	{
+		m_plain_text[loop] = (uint8_t)(all[loop]);
+	}
+
+	printk("End copying to all!\n");
+	printk("all: %s", all);
+
+	encryptData(all, strlen(all));
+
+	//printk("Encrypt Ended!\n");
+
+	//encryptData(all, strlen(all));
+
+}
+
 /*
 * Multicell Location
 *
@@ -1017,22 +1376,28 @@ void getSNR(int towerNum)
 			}
 		}
 
-		//printk("SNR: %s", data);
+		printk("SNR: %s", data);
 
 		//if((strcmp(t1ID, "empty") == 0))
 		if(towerNum == 1)
 		{
-			strcpy(t1SNR, data);
+			strcat(t1SNR, " ");
+			strcat(t1SNR, data);
+			//strcpy(t1SNR, data);
 		}	
 		//else if((strcmp(t2ID, "empty") == 0))
 		else if(towerNum == 2)
 		{
-			strcpy(t2SNR, data);
+			strcat(t2SNR, " ");
+			strcat(t2SNR, data);
+			//strcpy(t2SNR, data);
 		}
 		//else if((strcmp(t3ID, "empty") == 0))
 		else if(towerNum == 3)
 		{
-			strcpy(t3SNR, data);
+			strcat(t3SNR, " ");
+			strcat(t3SNR, data);
+			//strcpy(t3SNR, data);
 		}
 		else
 		{
@@ -1069,9 +1434,12 @@ static void print_cell_data(void)
 	char newTA[20];
 	sprintf(newTA,"%d",cell_data.current_cell.timing_advance);
 	char newCOPS[25];
-	sprintf(newCOPS,"%03d",cell_data.current_cell.mcc);
+	char newMCC[13];
+	sprintf(newMCC,"%03d",cell_data.current_cell.mcc);
+	//sprintf(newCOPS,"%03d",cell_data.current_cell.mcc);
 	char newMNC[13];
 	sprintf(newMNC,"%03d",cell_data.current_cell.mnc);
+	strcpy(newCOPS,newMCC);
 	strcat(newCOPS,newMNC);
 	
 	//make sure timing advance is a valid number
@@ -1097,7 +1465,7 @@ static void print_cell_data(void)
 		}
 
 		//else if((strstr(t2ID, "empty") != NULL) && (!(strstr(t1ID, newID) != NULL) || !(strstr(t1COPS, newCOPS) != NULL) )) // && !(strstr(newTA, "65535") != NULL)
-		else if((strstr(t2ID, "empty") != NULL) && (!(strstr(t1ID, newID) != NULL)))
+		else if((strstr(t2ID, "empty") != NULL)  && (!(strstr(t1COPS, newCOPS) != NULL))) //&& (!(strstr(t1ID, newID) != NULL))
 		{	
 			//strcpy(t2ID,cell_data.current_cell.id);
 			sprintf(t2ID,"%d",cell_data.current_cell.id);
@@ -1114,7 +1482,7 @@ static void print_cell_data(void)
 		}
 
 		//else if((strstr(t3ID, "empty") != NULL) && (!(strstr(t2ID, newID) != NULL) || !(strstr(t2COPS, newCOPS) != NULL) ) && (!(strstr(t1ID, newID) != NULL) || !(strstr(t1COPS, newCOPS) != NULL) ) ) //&& !(strstr(newTA, "65535") != NULL)
-		else if((strstr(t3ID, "empty") != NULL) && (!(strstr(t2ID, newID) != NULL)) && (!(strstr(t1ID, newID) != NULL)))
+		else if((strstr(t3ID, "empty") != NULL) && (!(strstr(t1COPS, newCOPS) != NULL)) && (!(strstr(t2COPS, newCOPS) != NULL))) //(!(strstr(t2ID, newID) != NULL)) && (!(strstr(t1ID, newID) != NULL))
 		{	
 			//strcpy(t3ID,cell_data.current_cell.id);
 			sprintf(t3ID,"%d",cell_data.current_cell.id);
@@ -1259,6 +1627,7 @@ void getTime(void)
 		}
 
 		strcpy(time,data);	
+		printk("Time: %s", time);
 
     //printk("Modem response:\n%s", response);
 }
@@ -1295,6 +1664,8 @@ static void button_handler(uint32_t button_states, uint32_t has_changed)
 
 		LOG_INF("Button 1 pressed, starting cell measurements");
 		start_cell_measurements();
+
+		getTime();
 	
 		//readCOPS();
 		//testCOPS();
@@ -1402,6 +1773,23 @@ static void button_handler(uint32_t button_states, uint32_t has_changed)
 		
 		AppendString(testTxt, time, strlen(time), false);
 		AppendString(testTxt, separator, strlen(separator), false);
+
+		/*
+		secure();
+
+		startCrypto();
+
+		decryptData();
+
+		cryptoEnd();
+
+		int loop;
+		for(loop = 0; loop < (sizeof(m_encrypted_text) / sizeof(m_encrypted_text[0])); loop++)
+		{
+			printk("%d", m_encrypted_text[loop]);
+		}
+		printk("\n");
+		*/
 		
 		empty();
 
@@ -1604,8 +1992,9 @@ void main(void)
 {
 	//initialize storage
 	//blinkTimes(1);
-	storage();
 	
+	storage();
+
 	int startFakeLEDs;
 	startFakeLEDs = gpio_pin_set_dt(&Tower_Read_Success, LOW);
 	startFakeLEDs = gpio_pin_set_dt(&Tower_Read_Fail, LOW);
